bb61b30f5c33904dba50865b2037c7f9e807947c
marcbgold <marcbgold2@gmail.com>
2018-04-01 11:19:06 -0400

    wrote test that player hp should go down when player takes damage

--- src/test/java/org/wecancodeit/tradingcardgame/PlayerTest.java
+++ src/test/java/org/wecancodeit/tradingcardgame/PlayerTest.java
@@ -3,17 +3,30 @@ package org.wecancodeit.tradingcardgame;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PlayerTest {
 
 	Player underTest;
 
+	@Before
+	public void setup() {
+		underTest = new Player(30);
+	}
+
 	@Test
 	public void playerShouldHaveHp() {
 		underTest = new Player(30);
 
 		assertThat(underTest.getHp(), is(30));
+	}
+
+	@Test
+	public void playerHpShouldGoDownWhenPlayerTakesDamage() {
+		underTest.takeDamage(1);
+
+		assertThat(underTest.getHp(), is(29));
 	}
 
 }
