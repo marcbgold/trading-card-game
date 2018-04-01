d84755619e6ae13e684cc5a677f8f51a7bacfb9e
marcbgold <marcbgold2@gmail.com>
2018-04-01 11:05:18 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/tradingcardgame/TradingCardGameApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.tradingcardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingCardGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingCardGameApplication.class, args);
+	}
+}
