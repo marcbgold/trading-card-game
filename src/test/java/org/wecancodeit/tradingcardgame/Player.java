c8efb955e2864b8334ccbfba3ba9034f9cf242c0
marcbgold <marcbgold2@gmail.com>
2018-04-01 11:16:40 -0400

    created player class with hp, player hp test passes

---
+++ src/test/java/org/wecancodeit/tradingcardgame/Player.java
@@ -0,0 +1,15 @@
+package org.wecancodeit.tradingcardgame;
+
+public class Player {
+
+	private int hp;
+
+	public Player(int hp) {
+		this.hp = hp;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+}
